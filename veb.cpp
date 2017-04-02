af25aa556434ddf58b28a65309b99d6136a1a06d
Vlad <VladYaglamunov@gmail.com>
2017-04-03 02:29:39 +0300

    first commit

---
+++ veb.cpp
@@ -0,0 +1,152 @@
+#include <array>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "veb.h"
+
+template <unsigned int S>
+class VEBTree : AbstractVEBTree<S> {
+   private:
+    const static int_value half = (1ULL << (S / 2)) - 1;
+
+    std::array<VEBTree<S / 2>*, 1ULL << (S / 2)> children;
+    VEBTree<S / 2>* aux;
+
+    int_value min, max;
+
+    static int_value high(int_value x) {
+        return x & (~half);
+    }
+
+    static int_value low(int_value x) {
+        return x & half;
+    }
+
+    static int_value merge(int_value high, int_value low) {
+        return low | high;
+    }
+
+   public:
+    VEBTree() {
+        min = NO;
+        max = NO;
+        if (children.size() > 2) {
+            aux = new VEBTree<S / 2>();
+            for (auto& child : children) {
+                child = new VEBTree<S / 2>();  // TODO: maybe some lazy shit???
+            }
+        }
+    }
+
+    bool empty() const {
+        return min == NO;
+    }
+
+    void add(int_value x) {
+        if (empty()) {
+            min = x;
+            max = x;
+        } else if (max == min) {
+            if (x < min) {
+                min = x;
+            } else {
+                max = x;
+            }
+        } else {
+            if (x < min) std::swap(min, x);
+            if (max < x) std::swap(max, x);
+
+            if (S != 1) {
+                if (children[high(x)]->empty()) aux->add(high(x));
+                children[high(x)]->add(low(x));
+            }
+        }
+    }
+
+    void remove(int_value x) {
+        if (empty()) return;
+        if (min == max) {
+            min = NO;
+        } else if (min == x) {
+            if (aux->empty()) {
+                min = max;
+                return;
+            }
+            min = merge(aux->getMin(), children[aux->getMin()]->getMin());
+        } else if (max == x) {
+            if (aux->empty()) {
+                max = min;
+                return;
+            }
+            max = merge(aux->getMax(), children[aux->getMax()]->getMax());
+        } else if (aux->empty()) {
+            return;
+        } else {
+            children[high(x)]->remove(low(x));
+            if (children[high(x)]->empty()) {
+                aux->remove(high(x));
+            }
+        }
+    }
+
+    int_value next(int_value x) const {
+        if (empty()) return NO;
+        if (x < min) return min;
+        if (aux->empty()) return max;
+
+        if (!children[high(x)]->empty() && children[high(x)]->getMax() > low(x))
+            return merge(high(x), children[high(x)]->next(low(x)));
+
+        int_value next_high = aux->next(high(x));
+        if (next_high == NO) {
+            return NO;
+        } else {
+            return merge(next_high, children[next_high]->getMax());
+        }
+    }
+
+    int_value prev(int_value x) const {
+        if (empty()) return NO;
+        if (max < x) return max;
+        if (aux->empty()) return min;
+
+        if (!children[high(x)]->empty() && children[high(x)]->getMin() < low(x))
+            return merge(high(x), children[high(x)]->prev(low(x)));
+
+        int_value prev_high = aux->prev(high(x));
+        if (prev_high == NO) {
+            return NO;
+        } else {
+            return merge(prev_high, children[prev_high]->getMin());
+        }
+    }
+
+    int_value getMin() const {
+        return min;
+    }
+
+    int_value getMax() const {
+        return max;
+    }
+};
+
+int main() {
+    VEBTree<10> tree;
+
+    std::cout << tree.empty() << std::endl;
+
+    tree.add(5);
+    tree.add(11);
+    tree.add(10);
+
+    std::cout << tree.empty() << std::endl;
+
+    std::cout << tree.next(5) << std::endl;
+    std::cout << tree.prev(11) << std::endl;
+    tree.remove(10);
+    std::cout << tree.next(5) << std::endl;
+    std::cout << tree.prev(11) << std::endl;
+
+    return 0;
+}
---
+++ veb.h
@@ -0,0 +1,22 @@
+#ifndef VEB_H
+#define VEB_H
+
+typedef unsigned long long int_value;
+
+int_value const NO = -1;
+
+template<unsigned int S>
+class AbstractVEBTree {
+    public:
+        virtual void add(int_value x) = 0;
+        virtual void remove(int_value x) = 0;
+        virtual int_value next(int_value x) const = 0;
+        virtual int_value prev(int_value x) const = 0;
+        virtual int_value getMin() const = 0;
+        virtual int_value getMax() const = 0;
+};
+
+template<unsigned int S>
+class VEBTree;
+
+#endif
