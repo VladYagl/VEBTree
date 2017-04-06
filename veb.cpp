e99600545fa8142e44b7d1a77db40091ff4c7cef
Vlad <VladYaglamunov@gmail.com>
2017-04-06 18:49:13 +0300

    well now it's broken Kappa

--- veb.cpp
+++ veb.cpp
@@ -7,158 +7,191 @@
 
 template <unsigned int S>
 class VEBTree : AbstractVEBTree<S> {
-   private:
-    const static int_value half = (1ULL << ((S + 1) / 2)) - 1;
+private:
+	int ss = S;
+	const static int_value half = (1ULL << (S / 2)) - 1;
+	const static unsigned int HALF = (S + 1) / 2;
 
-    std::array<VEBTree<S / 2>*, 1ULL << (S / 2)> children;
-    VEBTree<S / 2>* aux;
+	std::array<VEBTree<S / 2 + 1>*, 1ULL << HALF> children;
+	VEBTree<S / 2 + 1>* aux;
 
-    int_value min, max;
+	int_value min, max;
 
-    static int_value high(int_value x) {
-        return (x & (~half)) >> ((S + 1) / 2);
-    }
+	static int_value high(int_value x) {
+		return (x) >> (S / 2);
+	}
 
-    static int_value low(int_value x) {
-        return x & half;
-    }
+	static int_value low(int_value x) {
+		return x & half;
+	}
 
-    static int_value merge(int_value high, int_value low) {
-        return low | high;
-    }
+	static int_value merge(int_value high, int_value low) {
+		return low | (high << (S / 2));
+	}
 
-   public:
-    VEBTree() {
-        min = NO;
-        max = NO;
-        if (S > 1) {
-            aux = new VEBTree<S / 2>();
-            for (auto& child : children) {
-                child = new VEBTree<S / 2>();  // TODO: maybe some lazy shit???
-            }
-        }
-    }
+public:
+	VEBTree() {
+		min = NO;
+		max = NO;
+		if (S > 1) {
+			aux = new VEBTree<S / 2 + 1>();
+			for (auto& child : children) {
+				child = new VEBTree<S / 2 + 1>();  // TODO: maybe some lazy shit???
+			}
+		}
+	}
 
-    bool empty() const {
-        return min == NO;
-    }
+	virtual ~VEBTree() {
+		for (auto& i : children) {
+			delete i;
+		}
+		delete aux;
+	}
 
-    void add(int_value x) {
-        std::cout << "   add start"<<x<<"\n";
-        if (empty()) {
-            min = x;
-            max = x;
-        } else if (max == min) {
-            if (x < min) {
-                min = x;
-            } else {
-                max = x;
-            }
-        } else {
-            std::cout << "   add recursive, current S = " << S << '\n';
-            if (x < min) std::swap(min, x);
-            if (max < x) std::swap(max, x);
+	bool empty() const {
+		return min == NO;
+	}
 
-            if (S > 2) {
-                std::cout << "this is if " << half << ' ' << x << ' ' << high(x) << ' ' << low(x) << "\n";
-                if (children[high(x)]->empty()) aux->add(high(x));
-                children[high(x)]->add(low(x));
-                std::cout << "this is end if \n";
-            }
-        }
-        std::cout << "  add end\n";
-    }
+	void add(int_value x) override {
+		if (empty()) {
+			min = x;
+			max = x;
+		} else if (max == min) {
+			if (x < min) {
+				min = x;
+			} else {
+				max = x;
+			}
+		} else {
+			if (x < min) std::swap(min, x);
+			if (max < x) std::swap(max, x);
 
-    void remove(int_value x) {
-        if (empty()) return;
-        if (min == max) {
-            min = NO;
-        } else if (min == x) {
-            if (aux->empty()) {
-                min = max;
-                return;
-            }
-            min = merge(aux->getMin(), children[aux->getMin()]->getMin());
-        } else if (max == x) {
-            if (aux->empty()) {
-                max = min;
-                return;
-            }
-            max = merge(aux->getMax(), children[aux->getMax()]->getMax());
-        } else if (aux->empty()) {
-            return;
-        } else {
-            children[high(x)]->remove(low(x));
-            if (children[high(x)]->empty()) {
-                aux->remove(high(x));
-            }
-        }
-    }
+			if (S > 1) {
+				if (children[high(x)]->empty()) {
+					aux->add(high(x));
+				}
+				children[high(x)]->add(low(x));
+			}
+		}
+	}
 
-    int_value next(int_value x) const {
-        if (empty()) return NO;
-        if (x < min) return min;
-        if (aux->empty()) return max;
+	void remove(int_value x) override {
+		if (empty()) return;
+		if (min == max) {
+			min = NO;
+		} else if (min == x) {
+			if (aux->empty()) {
+				min = max;
+				return;
+			}
+			min = merge(aux->getMin(), children[aux->getMin()]->getMin());
+		} else if (max == x) {
+			if (aux->empty()) {
+				max = min;
+				return;
+			}
+			max = merge(aux->getMax(), children[aux->getMax()]->getMax());
+		} else if (aux->empty()) {
+			return;
+		} else {
+			children[high(x)]->remove(low(x));
+			if (children[high(x)]->empty()) {
+				aux->remove(high(x));
+			}
+		}
+	}
 
-        if (!children[high(x)]->empty() && children[high(x)]->getMax() > low(x)) return merge(high(x), children[high(x)]->next(low(x)));
+	int_value next(int_value x) const override {
+		if (empty()) return NO;
+		if (x < min) return min;
+		if (S == 1 || aux->empty()) return max;
 
-        int_value next_high = aux->next(high(x));
-        if (next_high == NO) {
-            return NO;
-        } else {
-            return merge(next_high, children[next_high]->getMax());
-        }
-    }
+		if (!children[high(x)]->empty() && children[high(x)]->getMax() > low(x)) {
+			return merge(high(x), children[high(x)]->next(low(x)));
+		}
 
-    int_value prev(int_value x) const {
-        if (empty()) return NO;
-        if (max < x) return max;
-        if (aux->empty()) return min;
+		int_value next_high = aux->next(high(x));
+		if (next_high == NO) {
+			return NO;
+		} else if (next_high == high(x)) {
+			return max;
+		} else {
+			return merge(next_high, children[next_high]->getMin());
+		}
+	}
 
-        if (!children[high(x)]->empty() && children[high(x)]->getMin() < low(x)) return merge(high(x), children[high(x)]->prev(low(x)));
+	int_value prev(int_value x) const override {
+		if (empty()) return NO;
+		if (max < x) return max;
+		if (S == 1 || aux->empty()) return min;
 
-        int_value prev_high = aux->prev(high(x));
-        if (prev_high == NO) {
-            return NO;
-        } else {
-            return merge(prev_high, children[prev_high]->getMin());
-        }
-    }
+		if (!children[high(x)]->empty() && children[high(x)]->getMin() < low(x)) {
+			return merge(high(x), children[high(x)]->prev(low(x)));
+		}
 
-    int_value getMin() const {
-        return min;
-    }
+		int_value prev_high = aux->prev(high(x));
+		if (prev_high == NO) {
+			return NO;
+		} else if (prev_high == high(x)) {
+			return min;
+		} else {
+			return merge(prev_high, children[prev_high]->getMax());
+		}
+	}
 
-    int_value getMax() const {
-        return max;
-    }
+	int_value getMin() const override {
+		return min;
+	}
+
+	int_value getMax() const override {
+		return max;
+	}
 };
 
+#include <set>
+
 int main() {
-    VEBTree<10> tree;
+	/*VEBTree<10> tree;
 
-    std::cout << tree.empty() << std::endl;
+	std::cout << tree.empty() << std::endl;
 
-    tree.add(5);
-    tree.add(11);
-    tree.add(10);
+	tree.add(5);
+	tree.add(11);
+	tree.add(10);
 
-    std::cout << tree.empty() << std::endl;
+	std::cout << tree.empty() << std::endl;
 
-    std::cout << tree.next(5) << std::endl;
-    std::cout << tree.prev(11) << std::endl;
-    tree.remove(10);
-    std::cout << tree.next(5) << std::endl;
-    std::cout << tree.prev(11) << std::endl;
+	std::cout << tree.next(5) << std::endl;
+	std::cout << tree.prev(11) << std::endl;
+	tree.remove(10);
+	std::cout << tree.next(5) << std::endl;
+	std::cout << tree.prev(11) << std::endl;
 
-    std::cout << "start shirv test" << std::endl;
+	std::cout << "start shirv test" << std::endl;
+*/
+	VEBTree<5> a;
+	std::set<int_value> s;
 
-    VEBTree<10> a;
-    for (int i = 0; i < 50; i += 10) {
-        std::cout << "add " << i << std::endl;
-        a.add(i);
-        std::cout << "prev " << a.prev(i) << std::endl;
-    }
+	for (int i = 0; i < 500; i++) {
+		int_value x = (rand() * RAND_MAX + rand()) % (1 << 5);
+		a.add(x);
+		s.insert(x);
+		auto temp = s.find(x);
+		auto curr = temp;
+		auto temp2 = temp;
+		auto next = ++temp;
+		auto prev = --temp2;
+		auto pp = a.prev(x);
+		auto nn = a.next(x);
+		if (curr != s.begin() && a.prev(x) != *prev) {
+			std::cout << x << std::endl;
+		}
+		if (next != s.end() && a.next(x) != *next) {
+			std::cout << x << std::endl;
+		}
+	}
 
-    return 0;
+	std::cout << "OK\n";
+
+	return 0;
 }
--- veb.h
+++ veb.h
@@ -8,7 +8,9 @@ int_value const NO = -1;
 template<unsigned int S>
 class AbstractVEBTree {
     public:
-        virtual void add(int_value x) = 0;
+	virtual ~AbstractVEBTree() {}
+
+	virtual void add(int_value x) = 0;
         virtual void remove(int_value x) = 0;
         virtual int_value next(int_value x) const = 0;
         virtual int_value prev(int_value x) const = 0;
