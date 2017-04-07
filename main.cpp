8b43910d50a7872554fa39cc0155f680ad15ac50
Vlad <VladYaglamunov@gmail.com>
2017-04-07 16:42:30 +0300

    PogChamp I'm not gonna lie this definitely me when it's done.

--- main.cpp
+++ main.cpp
@@ -18,54 +18,44 @@ int main() {
 
     for (int i = 0; i < ITERATIONS; i++) {
         int_value x = rand() % (1 << TREE_SIZE);
-        /* cout << endl << "******begin i = " << i << " x = " << x; */
-        try {
-            if (rand() % 50 == 0) {
-                /* cout << " remove"; */
-                a->remove(x);
-                s.erase(x);
-                /* cout << "+" << endl; */
-            } else {
-                /* cout << " add"; */
-                a->add(x);
-                s.insert(x);
-                /* cout << "+" << endl; */
-            }
-            auto temp = s.lower_bound(x);
-            auto curr = temp;
-            auto temp2 = temp;
-            auto next = temp;
-            if (next != s.end() && *next == x) ++next;
-            auto prev = temp2;
-            if (curr != s.begin()) --prev;
+        if (i % 1000 == 0) {
+            cout << endl << "******begin i = " << i << " x = " << x;
+        }
+        if (rand() % 50 == 0) {
+            /* cout << " remove"; */
+            a->remove(x);
+            s.erase(x);
+            /* cout << "+" << endl; */
+        } else {
+            /* cout << " add"; */
+            a->add(x);
+            s.insert(x);
+            /* cout << "+" << endl; */
+        }
+        auto temp = s.lower_bound(x);
+        auto curr = temp;
+        auto temp2 = temp;
+        auto next = temp;
+        if (next != s.end() && *next == x) ++next;
+        auto prev = temp2;
+        if (curr != s.begin()) --prev;
 
-            if (curr != s.begin() && a->prev(x) != *prev) {
-                std::cout << "prev mistake i = " << i << " x = " << x << ' ' << a->prev(x) << "!=" << *prev << std::endl;
-                for (auto i : s) {
-                    std::cout << i << ' ';
-                }
-                std::cout << std::endl << std::endl;
-                a->print();
-                std::cout << std::endl << std::endl;
-                return 0;
+        if (curr != s.begin() && a->prev(x) != *prev) {
+            std::cout << "prev mistake i = " << i << " x = " << x << ' ' << a->prev(x) << "!=" << *prev << std::endl;
+            for (auto i : s) {
+                std::cout << i << ' ';
             }
-
-            if (next != s.end() && a->next(x) != *next) {
-                std::cout << "next mistake i = " << i << " x = " << x << ' ' << a->next(x) << "!=" << *next << std::endl;
-                for (auto i : s) {
-                    std::cout << i << ' ';
-                }
-                std::cout << std::endl << std::endl;
-                a->print();
-                std::cout << std::endl << std::endl;
-                return 0;
-            }
-
-            /* cout << "******end i = " << i << endl; */
-        } catch (Exception* e) {
-            e->print();
             return 0;
         }
+
+        if (next != s.end() && a->next(x) != *next) {
+            std::cout << "next mistake i = " << i << " x = " << x << ' ' << a->next(x) << "!=" << *next << std::endl;
+            for (auto i : s) {
+                std::cout << i << ' ';
+            }
+            return 0;
+        }
+        /* cout << "******end i = " << i << endl; */
     }
 
     std::cout << "OK\n";
--- veb.cpp
+++ veb.cpp
@@ -1,67 +1,14 @@
 #include <array>
-#include <iostream>
-#include <string>
-#include <vector>
 
 #include "veb.h"
-
-struct Exception {
-    bool aux = false;
-    Exception* child = nullptr;
-    std::string name = "Exception";
-    int_value x;
-    unsigned int S;
-
-    Exception(std::string name, int_value x, unsigned int S) : name(name), x(x), S(S) {
-    }
-
-    ~Exception() {
-        delete child;
-    }
-
-    void print() {
-        std::cout << std::endl;
-        std::cout << name << std::endl << "   ";
-        print_info();
-        std::cout << std::endl;
-    }
-
-   private:
-    int_value high(int_value x) {
-        return (x) >> (S / 2);
-    }
-
-    int_value low(int_value x) {
-        return x & ((1ULL << (S / 2)) - 1);
-    }
-
-    void print_info() {
-        std::cout << "  (x = " << x << " S = " << S << " high = " << high(x) << " low = " << low(x) << " size = " << (1ULL << ((S + 1) / 2)) << ") " << child
-                  << std::endl;
-        if (child != nullptr) {
-            if (aux) {
-                std::cout << "aux";
-            } else {
-                std::cout << "   ";
-            }
-            child->print_info();
-        }
-    }
-};
 
 template <unsigned int S>
 class VEBTree : AbstractVEBTree<S> {
    private:
-    bool initialized = false;
-
     std::array<VEBTree<S / 2>*, 1ULL << ((S + 1) / 2)> children;
     VEBTree<(S + 1) / 2>* aux;
 
     int_value min, max;
-
-    // DEBUG
-   public:
-    // DEBUG
 
     static int_value high(int_value x) {
         return (x) >> (S / 2);
@@ -80,9 +27,6 @@ class VEBTree : AbstractVEBTree<S> {
         max = NO;
         if (S > 1) {
             aux = new VEBTree<(S + 1) / 2>();
-            for (auto& child : children) {
-                child = new VEBTree<S / 2>();  // TODO: maybe some lazy shit???
-            }
         }
     }
 
@@ -98,8 +42,6 @@ class VEBTree : AbstractVEBTree<S> {
     }
 
     void add(int_value x) override {
-        /* std::cout << std::endl; */
-        /* print(); */
         if (empty()) {
             min = x;
             max = x;
@@ -115,42 +57,19 @@ class VEBTree : AbstractVEBTree<S> {
             if (max < x) std::swap(max, x);
 
             if (S > 1) {
-                // DEBUG
-                if (high(x) >= children.size()) {
-                    throw new Exception("Add error", x, S);
+                if (children[high(x)] == nullptr) {
+                    children[high(x)] = new VEBTree<S / 2>();
                 }
-                // DEBUG
 
                 if (children[high(x)]->empty()) {
-                    // DEBUG
-                    try {
-                        aux->add(high(x));
-                    } catch (Exception* e) {
-                        Exception* tmp = new Exception("Add error", x, S);
-                        tmp->child = e;
-                        tmp->aux = true;
-                        throw tmp;
-                    }
-                    // DEBUG
-                    /* aux->add(high(x)); */
+                    aux->add(high(x));
                 }
-                // DEBUG
-                try {
-                    children[high(x)]->add(low(x));
-                } catch (Exception* e) {
-                    Exception* tmp = new Exception("Add error", x, S);
-                    tmp->child = e;
-                    throw tmp;
-                }
-                // DEBUG
-                /* children[high(x)]->add(low(x)); */
+                children[high(x)]->add(low(x));
             }
         }
     }
 
     void remove(int_value x) override {
-        /* std::cout << std::endl; */
-        /* print(); */
         if (empty()) return;
         if (min == max) {
             if (min == x) min = NO;
@@ -177,70 +96,31 @@ class VEBTree : AbstractVEBTree<S> {
         } else if (S == 1 || aux->empty()) {
             return;
         } else {
-            children[high(x)]->remove(low(x));
-            if (children[high(x)]->empty()) {
-                aux->remove(high(x));
+            if (children[high(x)] != nullptr) {
+                children[high(x)]->remove(low(x));
+                if (children[high(x)]->empty()) {
+                    aux->remove(high(x));
+                }
             }
         }
     }
 
     int_value next(int_value x) const override {
-        /* std::cout << "get_next : S = " << S << " x = " << x << std::endl; */
-        int_value result = next_impl(x);
-        /* std::cout << "result = " << result << std::endl; */
-        return result;
-    }
-
-    int_value next_impl(int_value x) const {
         if (empty()) return NO;
         if (x < min) return min;
         if (S == 1 || aux->empty()) return max;
 
-        // DEBUG
-        if (high(x) >= children.size()) {
-            throw new Exception("Next error", x, S);
-        }
-        // DEBUG
-
-        if (!children[high(x)]->empty() && children[high(x)]->getMax() > low(x)) {
-            // DEBUG
-            try {
-                return merge(high(x), children[high(x)]->next(low(x)));
-            } catch (Exception* e) {
-                Exception* tmp = new Exception("Next error", x, S);
-                tmp->child = e;
-                throw tmp;
-            }
-            // DEBUG
-            /* return merge(high(x), children[high(x)]->next(low(x))); */
+        if (children[high(x)] != nullptr && !children[high(x)]->empty() && children[high(x)]->getMax() > low(x)) {
+            return merge(high(x), children[high(x)]->next(low(x)));
         }
 
-        // DEBUG
-        /* aux->print(1, "   aux   "); */
-        int_value next_high = NO;
-        try {
-            next_high = aux->next(high(x));
-        } catch (Exception* e) {
-            Exception* tmp = new Exception("Next error", x, S);
-            tmp->child = e;
-            tmp->aux = true;
-            throw tmp;
-        }
-        /* std::cout <<  "   next_high = " << next_high << " x = " << x << " high(x) = " << high(x) << std::endl; */
-        // DEBUG
-        /* int_value next_high = aux->next(high(x)); */
+        int_value next_high = aux->next(high(x));
         if (next_high == NO) {
             return NO;
         } else if (next_high <= high(x)) {
             return max;
         } else {
-            try {
-                return merge(next_high, children[next_high]->getMin());
-            } catch (Exception* e) {
-                Exception* tmp = new Exception("Next error", x, S);
-                tmp->child = e;
-                throw tmp;
-            }
+            return merge(next_high, children[next_high]->getMin());
         }
     }
 
@@ -249,7 +129,7 @@ class VEBTree : AbstractVEBTree<S> {
         if (max < x) return max;
         if (S == 1 || aux->empty()) return min;
 
-        if (!children[high(x)]->empty() && children[high(x)]->getMin() < low(x)) {
+        if (children[high(x)] != nullptr && !children[high(x)]->empty() && children[high(x)]->getMin() < low(x)) {
             return merge(high(x), children[high(x)]->prev(low(x)));
         }
 
@@ -270,21 +150,4 @@ class VEBTree : AbstractVEBTree<S> {
     int_value getMax() const override {
         return max;
     }
-
-    // DEBUG
-    void print(int_value h, std::string tab) {
-        std::cout << tab << "(" << h << ")S = " << S << " min = " << h + min << " max = " << h + max << std::endl;
-        if (S > 1) {
-            for (int i = 0; i < children.size(); i++) {
-                if (!children[i]->empty()) {
-                    children[i]->print(h + (i << (S / 2)), tab + "  ");
-                }
-            }
-        }
-    }
-
-    void print() {
-        print(0, "");
-    }
-    // DEBUG
 };
